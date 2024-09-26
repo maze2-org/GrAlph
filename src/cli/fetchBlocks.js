3133ce9b6e8642eb1f04b672a9e98b173a6226bc
Aram <aramsuqiasyan20@gmail.com>
2024-09-26 14:28:10 +0400

    feat: cli command for getting blocks and save

--- src/cli/fetchBlocks.js
+++ src/cli/fetchBlocks.js
@@ -4,46 +4,54 @@ const Block = require('../models/Block');
 const { Command } = require('commander');
 const program = new Command();
 
-program
-  .command('fetch-blocks')
-  .description('Fetch blockchain blocks from Alephium and save to MongoDB')
-  .option('--fromTs <fromTs>', 'Start timestamp for blocks', parseInt)
-  .option('--toTs <toTs>', 'End timestamp for blocks', parseInt)
-  .action(async (cmdObj) => {
-    const { fromTs, toTs } = cmdObj;
-    console.log('fromTs',fromTs)
-    console.log('toTs',toTs)
-    if (!fromTs || !toTs) {
-      console.error('Please provide both fromTs and toTs as parameters.');
-      return;
-    }
-    try {
-      // Получение блоков с API Alephium
-      const {data} = await axios.get('https://node-v20.mainnet.alephium.org/blockflow/blocks-with-events', {
-        params: {fromTs, toTs}
+async function fetchBlocks(fromTs) {
+  if (!fromTs) {
+    console.error('❌Please provide fromTs as a parameter.');
+    return;
+  }
+  try {
+    while (true) {
+
+      const toTs = fromTs + 1800000;
+
+      const { data } = await axios.get('https://node-v20.mainnet.alephium.org/blockflow/blocks-with-events', {
+        params: { fromTs, toTs }
       });
 
       const blocks = data?.blocksAndEvents?.flatMap(blockArray => blockArray.map(blockItem => blockItem.block)) || [];
-      // console.log('blocks',blocks)
-
-      console.log(JSON.stringify(blocks[0],null,2))
 
       if (blocks.length === 0) {
-        console.log('No blocks found for the specified time range.');
-        return;
+        console.log('⚠️No blocks found for the specified time range.');
+        continue;
       }
 
-      // Подключение к MongoDB
       await mongoose.connect('mongodb://localhost:27017/alephium');
 
-      // Сохранение отфильтрованных блоков в MongoDB
       await Block.insertMany(blocks);
-      console.log(`${blocks.length} blocks saved to MongoDB`);
 
-      mongoose.connection.close();
-    } catch (error) {
-      console.error('Error fetching or saving blocks:', error);
+      fromTs = toTs;
+
+      if (fromTs >= Date.now()) {
+        console.log('✅Reached the current time, stopping fetch.');
+        break;
+      }
+
+      await new Promise(resolve => setTimeout(resolve, 5000));
     }
+  } catch (error) {
+    console.error('❌Error fetching or saving blocks:', error);
+  } finally {
+    mongoose.connection.close().catch();
+  }
+}
+
+program
+  .command('fetch-blocks')
+  .description('Fetch blockchain blocks from Alephium and save to MongoDB')
+  .option('--fromTs <fromTs>', 'Start timestamp for blocks in milliseconds', parseInt)
+  .action(async (cmdObj) => {
+    const { fromTs } = cmdObj;
+    await fetchBlocks(fromTs);
   });
 
 program.parse(process.argv);
